71b8648ce76c12d0fd55701a0e504e3cd505a953
Babuze <134912091+Babuze1@users.noreply.github.com>
2023-08-22 20:18:28 +0300

    Completed assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for index, dwarf in enumerate(dwarves, start=1):
+        print(f"{index}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    capitalized_calls = [call.capitalize() + "!" for call in planeteer_calls]
+    return capitalized_calls
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(strings):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for item in strings:
+        if item in cheeses:
+            return item
+    return None
